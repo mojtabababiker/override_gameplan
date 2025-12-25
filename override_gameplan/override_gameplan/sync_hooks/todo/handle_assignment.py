5417d3dc48441e6a0763ffd2b5150ad4bdcb9e12
mojtabababiker <mojmohammad98@gmail.com>
2025-12-25 13:53:50 +0200

    feat: adding task assignment hook to sync between gameplane and erpnext

--- override_gameplan/override_gameplan/sync_hooks/todo/handle_assignment.py
+++ override_gameplan/override_gameplan/sync_hooks/todo/handle_assignment.py
@@ -1,6 +1,8 @@
 """Hook on ToDo assignment to inject the related extensions hooks"""
 
 import frappe
+
+from ..task import assign_gp_task_to_user
 from ..team import on_customer_update
 from ..project import on_project_update
 
@@ -8,16 +10,13 @@ from ..project import on_project_update
 MONITORED_DOCTYPES_HOOKS: dict[str, list[callable]] = {
     "Customer": [on_customer_update],
     "Project": [on_project_update],
+    "Task": [assign_gp_task_to_user],
 }
 
 
 def handle_assignment(doc, method=None):
     """This function is used to check"""
     # when a ToDo is updated to cancelled, we trigger the on_update hooks of the referenced documents, otherwise we do nothing
-    if not doc.has_value_changed("document_status"):
-        return
-    if doc.document_status != "Cancelled":
-        return
     if doc.reference_type in MONITORED_DOCTYPES_HOOKS:
         ref_doc = frappe.get_doc(doc.reference_type, doc.reference_name)
         for hook in MONITORED_DOCTYPES_HOOKS[doc.reference_type]:
@@ -26,6 +25,10 @@ def handle_assignment(doc, method=None):
 
 def handle_assignment_removal(doc, method=None):
     """This function is used to check"""
+    if not doc.has_value_changed("status"):
+        return
+    if doc.status != "Cancelled":
+        return
     if doc.reference_type in MONITORED_DOCTYPES_HOOKS:
         for hook in MONITORED_DOCTYPES_HOOKS[doc.reference_type]:
             ref_doc = frappe.get_doc(doc.reference_type, doc.reference_name)
